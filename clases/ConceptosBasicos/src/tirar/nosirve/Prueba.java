1adce743f8b9ad44a16adbd6b740ded905c6bbab
jeshc <hcjesus@gmail.com>
2022-03-01 11:21:35 -0600

    solo para demostrar uso de l bash

---
+++ clases/ConceptosBasicos/src/tirar/nosirve/Prueba.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tirar.nosirve;
+
+/**
+ *
+ * @author jeshc
+ */
+public class Prueba {
+    private int horas;
+
+    public Prueba() {
+    }
+
+    public Prueba(int horas) {
+        this.horas = horas;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+ 
+
+}
